64891662c87071d6b3d77f034df6b59d38440ab4
dxtuptown <mailcuakhanhdat@gmail.com>
2024-02-19 14:08:02 +0700

    #8-CRUDMovie

---
+++ SWP_BookingTicket/Areas/CinemaManager/Controllers/MovieController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SWP_BookingTicket.DataAccess.Repositories;
+using SWP_BookingTicket.Models;
+using SWP_BookingTicket.Services;
+using static Org.BouncyCastle.Crypto.Engines.SM2Engine;
+
+namespace SWP_BookingTicket.Areas.CinemaManager.Controllers
+{
+	[Area("CinemaManager")]
+	[Authorize(Roles = "cinemaManager")]
+	public class MovieController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IWebHostEnvironment _webHostEnvironment;
+		private readonly UploadImageService _uploadImageService;
+
+		public MovieController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment, UploadImageService uploadImageService)
+		{
+			_unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+			_uploadImageService = uploadImageService;
+
+        }
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			IEnumerable<Movie> movies = await _unitOfWork.Movie.GetAllAsync();
+            //return View(cinemas);
+            return View(movies);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Movie movie, IFormFile? fileImage)
+		{
+			if (ModelState.IsValid)
+			{
+				//string wwwRootPath = _webHostEnvironment.WebRootPath;
+				//if (fileImage != null)
+				//{
+				//	string fileNameImage = Guid.NewGuid().ToString() + Path.GetExtension(fileImage.FileName);
+				//	string MovieImagePath = Path.Combine(wwwRootPath, @"images\movie");
+				//	using (var fileStream = new FileStream(Path.Combine(MovieImagePath, fileNameImage), FileMode.Create))
+				//	{
+				//		fileImage.CopyTo(fileStream);
+
+				//	}
+				//	movie.ImageUrl = @"\images\movie\" + fileNameImage;
+
+				//}
+				movie.ImageUrl = _uploadImageService.UploadImage(fileImage, @"images\movie");
+				_unitOfWork.Movie.Add(movie);
+				_unitOfWork.Save();
+				TempData["success"] = "Procduct created succesfully";
+			}			
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public async Task<IActionResult> Update(Guid movie_id)
+		{
+			var movie = await _unitOfWork.Movie.GetFirstOrDefaultAsync(u => u.MovieID == movie_id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				return View(movie);
+			}
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(Movie movie, IFormFile? file)
+		{
+			if (ModelState.IsValid)
+			{
+				movie.ImageUrl = _uploadImageService.UploadImage(file, @"images\movie", movie.ImageUrl);
+				_unitOfWork.Movie.Update(movie);
+				_unitOfWork.Save();
+			}
+			return RedirectToAction("Index");
+		}
+		#region API Calls
+		[HttpGet]
+		public async Task<IActionResult> GetAllMovies()
+		{
+			var movies = await _unitOfWork.Movie.GetAllAsync();
+			return Json(new { data = movies });
+		}
+		[HttpDelete]
+		public async Task<IActionResult> Delete(Guid movie_id)
+		{
+			var movie = await _unitOfWork.Movie.GetFirstOrDefaultAsync( u=> u.MovieID == movie_id);
+			if (movie == null)
+			{
+				return Json(new { success = false, message = "Error while deleting" });
+			}
+			_unitOfWork.Movie.Delete(movie);
+			_unitOfWork.Save();
+			return Json(new { success = true, message = "Delete successfully! " });
+		}
+		#endregion
+	}
+}
---
+++ SWP_BookingTicket/wwwroot/js/movie.js
@@ -0,0 +1,65 @@
+﻿// Loading DataTable with Ajax
+var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#movieTable').DataTable({
+        "ajax": {
+            "url": "/CinemaManager/Movie/GetAllMovies"
+        },
+        "columns": [
+            { "data": "movieID", "width": "20%" },
+            { "data": "movieName", "width": "20%" },
+            { "data": "releaseDate", "width": "15%" },
+            { "data": "endDate", "width": "15%" },
+            {
+                "data": "movieID",
+                "render": function (data) {
+                    return `
+                       
+                            <a href="/CinemaManager/Movie/Update?movie_id=${data}"
+                            class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                            <a onClick=Delete('/CinemaManager/Movie/Delete?movie_id=${data}')
+                            class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+					   
+                        `
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+// SweetAlert library
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        Swal.fire(
+                            'Deleted!',
+                            'This movie has been deleted.',
+                            'success'
+                        )
+                        //toastr.success(data.message);
+                    } else {
+                        //toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
